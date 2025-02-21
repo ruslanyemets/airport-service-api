8aca4fb580646ceeba9423032d83a217a1b8b1c6
Ruslan <ruslanyemets@gmail.com>
2025-02-21 22:22:49 +0200

    Apply filtering to RouteViewSet and FlightViewSet

--- airport/views.py
+++ airport/views.py
@@ -1,4 +1,6 @@
-from django.db.models import F, Count
+from datetime import datetime
+
+from django.db.models import F, Count, Q
 from rest_framework import mixins
 from rest_framework.pagination import PageNumberPagination
 from rest_framework.permissions import IsAuthenticated
@@ -81,6 +83,28 @@ class RouteViewSet(
     serializer_class = RouteSerializer
     permission_classes = (IsAuthenticated,)
 
+    def get_queryset(self):
+        """Retrieve the routes with filters"""
+        source = self.request.query_params.get("source")
+        destination = self.request.query_params.get("destination")
+        airport = self.request.query_params.get("airport")
+
+        queryset = self.queryset
+
+        if source:
+            queryset = queryset.filter(source__name__icontains=source)
+
+        if destination:
+            queryset = queryset.filter(destination__name__icontains=destination)
+
+        if airport:
+            queryset = queryset.filter(
+                Q(source__name__icontains=airport)
+                | Q(destination__name__icontains=airport)
+            )
+
+        return queryset.distinct()
+
     def get_serializer_class(self):
         if self.action == "list":
             return RouteListSerializer
@@ -120,6 +144,29 @@ class FlightViewSet(
     )
     serializer_class = FlightSerializer
     permission_classes = (IsAuthenticated,)
+
+    def get_queryset(self):
+        """Retrieve the flights with filters"""
+        country = self.request.query_params.get("country")
+        route_id_str = self.request.query_params.get("route")
+        departure_time = self.request.query_params.get("departure_time")
+
+        queryset = self.queryset
+
+        if country:
+            queryset = queryset.filter(
+                Q(route__source__country__name__icontains=country)
+                | Q(route__destination__country__name__icontains=country)
+            )
+
+        if route_id_str:
+            queryset = queryset.filter(route__id=int(route_id_str))
+
+        if departure_time:
+            departure_time = datetime.strptime(departure_time, "%Y-%m-%d %H:%M")
+            queryset = queryset.filter(departure_time=departure_time)
+
+        return queryset.distinct()
 
     def get_serializer_class(self):
         if self.action == "list":
