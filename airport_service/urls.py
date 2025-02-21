62979b3b4d6f9e95d09126ca071731e389152a3b
Ruslan <ruslanyemets@gmail.com>
2025-02-21 15:47:50 +0200

    Add serializer, views and urls to user app, apply JWT-authentication to project

--- airport_service/urls.py
+++ airport_service/urls.py
@@ -4,4 +4,5 @@ from django.urls import path, include
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("api/cinema/", include("airport.urls", namespace="airport")),
+    path("api/user/", include("user.urls", namespace="user")),
 ]
