a6a5331e1220b070b192af95b21990eac7ede57b
Ruslan <ruslanyemets@gmail.com>
2025-03-31 13:22:21 +0300

    Change base endpoint names following RESTful principles

--- airport_service/urls.py
+++ airport_service/urls.py
@@ -11,16 +11,16 @@ from drf_spectacular.views import (
 
 urlpatterns = [
     path("admin/", admin.site.urls),
-    path("api/airport/", include("airport.urls", namespace="airport")),
-    path("api/user/", include("user.urls", namespace="user")),
+    path("api/airports/", include("airport.urls", namespace="airport")),
+    path("api/users/", include("user.urls", namespace="user")),
     path("api/schema/", SpectacularAPIView.as_view(), name="schema"),
     path(
-        "api/doc/swagger/",
+        "api/docs/swagger/",
         SpectacularSwaggerView.as_view(url_name="schema"),
         name="swagger-ui",
     ),
     path(
-        "api/doc/redoc/",
+        "api/docs/redoc/",
         SpectacularRedocView.as_view(url_name="schema"),
         name="redoc",
     ),
